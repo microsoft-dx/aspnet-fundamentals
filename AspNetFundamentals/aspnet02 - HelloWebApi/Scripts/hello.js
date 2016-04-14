15b580d49bc5c06a25e62e3edb021d79037f93c1
Radu Matei <matei.radu94@gmail.com>
2016-04-14 15:36:06 +0300

    Added ajax example uri parameters

---
+++ AspNetFundamentals/aspnet02 - HelloWebApi/Scripts/hello.js
@@ -0,0 +1,39 @@
+﻿var baseUrl = 'api/Hello/Greet'
+
+var simpleButton = $("#simpleButton");
+simpleButton.click(function () {
+
+    $.ajax({
+        url: baseUrl,
+        dataType: 'json',
+        success: alertMessage
+    });
+});
+
+var firstNameButton = $("#firstNameButton");
+firstNameButton.click(function () {
+    var url = baseUrl + '?name=' + $("#simpleInput").val();
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        success: alertMessage
+    });
+});
+
+
+var fullNameButton = $("#fullNameButton");
+fullNameButton.click(function () {
+    var url = baseUrl + '?firstName=' + $("#firstNameInput").val() + '&lastName=' + $("#lastNameInput").val();
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        success: alertMessage
+    });
+});
+
+
+function alertMessage(message) {
+    alert(message);
+}
