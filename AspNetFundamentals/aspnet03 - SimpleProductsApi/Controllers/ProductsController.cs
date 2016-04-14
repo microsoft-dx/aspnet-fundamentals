d2e04960fcfa6b949fa71e5183e4d1fdbb4cbf7c
Radu Matei <matei.radu94@gmail.com>
2016-04-14 16:07:54 +0300

    Added basic functionality for products controller

---
+++ AspNetFundamentals/aspnet03 - SimpleProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using SimpleProductsApi.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SimpleProductsApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product(1, "Onion", "Food", 13),
+            new Product(2, "Red Onion", "Food", 14)
+        };
+
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+    }
+}
