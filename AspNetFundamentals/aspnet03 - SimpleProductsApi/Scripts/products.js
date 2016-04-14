22d85a6b25bc2ae7f99223ab8f1a508f59bb9626
Radu Matei <matei.radu94@gmail.com>
2016-04-14 16:08:24 +0300

    Added ajax requests

---
+++ AspNetFundamentals/aspnet03 - SimpleProductsApi/Scripts/products.js
@@ -0,0 +1,17 @@
+﻿$.ajax({
+    url: 'api/Products/GetProducts',
+    dataType: 'json',
+    success: populateProductList
+});
+
+function populateProductList(products) {
+    $.each(products, function (index) {
+        var product = products[index];
+
+        addProductToList(product);
+    });
+}
+
+function addProductToList(product) {
+    $("#productsList").append('<li>' + product.Name + '</li>');
+}
