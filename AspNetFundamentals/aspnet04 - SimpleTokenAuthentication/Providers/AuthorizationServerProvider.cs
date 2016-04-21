71db02356a4dbad4623738a1d2bce3bd6120aaff
Radu Matei <matei.radu94@gmail.com>
2016-04-21 14:04:40 +0300

    Added support for bearer token authentication

---
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Providers/AuthorizationServerProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using SimpleTokenAuthentication.DAL;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SimpleTokenAuthentication.Providers
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (AuthenticationRepository authenticationRepository = new AuthenticationRepository())
+            {
+                IdentityUser user = await authenticationRepository.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "Incorrect user name or password");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+    }
+}
