410d455eda1de00dbd01660d420a1b9eae1a7119
Radu Matei <matei.radu94@gmail.com>
2016-04-21 13:57:26 +0300

    Added test controller

---
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using SimpleTokenAuthentication.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SimpleTokenAuthentication.Controllers
+{
+    [Authorize]
+    public class ProductsController : ApiController
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product(1, "Onion", "Food", 13),
+            new Product(2, "Red Onion", "Food", 14)
+        };
+
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+
+        [HttpPost]
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
---
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleTokenAuthentication.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, string category, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+    }
+}
