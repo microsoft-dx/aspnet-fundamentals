059ddb591a43fc3704b4ee45aff7a11d1e371501
Radu Matei <matei.radu94@gmail.com>
2016-04-21 13:57:10 +0300

    Added Account controller

---
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using SimpleTokenAuthentication.DAL;
+using SimpleTokenAuthentication.Models;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SimpleTokenAuthentication.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : ApiController
+    {
+        private AuthenticationRepository authenticationRepository;
+
+        public AccountController()
+        {
+            authenticationRepository = new AuthenticationRepository();
+        }
+
+        public async Task<IHttpActionResult> Register(User userModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            IdentityResult result = await authenticationRepository.RegisterUser(userModel);
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                authenticationRepository.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
