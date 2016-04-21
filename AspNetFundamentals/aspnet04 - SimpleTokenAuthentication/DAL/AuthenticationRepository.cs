79e466896f968dabf14ec20dfe49bf2d195b8001
Radu Matei <matei.radu94@gmail.com>
2016-04-21 13:56:49 +0300

    Adde AuthenticationRepository

---
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/DAL/AuthenticationRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SimpleTokenAuthentication.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SimpleTokenAuthentication.DAL
+{
+    public class AuthenticationRepository : IDisposable
+    {
+        private AuthenticationContext authenticationContext;
+        private UserManager<IdentityUser> userManager;
+
+        public AuthenticationRepository()
+        {
+            authenticationContext = new AuthenticationContext();
+            userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(authenticationContext));
+        }
+
+        public async Task<IdentityResult> RegisterUser(User userModel)
+        {
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = userModel.UserName
+            };
+
+            return await userManager.CreateAsync(user, userModel.Password);
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            return await userManager.FindAsync(userName, password);
+        }
+
+        public void Dispose()
+        {
+            authenticationContext.Dispose();
+            userManager.Dispose();
+        }
+    }
+}
