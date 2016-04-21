b22deb7123b786056112e0e12e54bb7c97367196
Radu Matei <matei.radu94@gmail.com>
2016-04-21 13:03:15 +0300

    Added Owin Startup class

---
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(SimpleTokenAuthentication.Startup))]
+
+namespace SimpleTokenAuthentication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
