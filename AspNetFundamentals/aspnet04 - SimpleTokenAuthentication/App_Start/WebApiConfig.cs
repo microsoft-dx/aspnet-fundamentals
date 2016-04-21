fa87233f6708aa88b2655d33240e327558e87080
Radu Matei <matei.radu94@gmail.com>
2016-04-21 13:03:24 +0300

    Added json formatter

--- AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/App_Start/WebApiConfig.cs
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
 
 namespace SimpleTokenAuthentication
 {
@@ -16,6 +19,9 @@ namespace SimpleTokenAuthentication
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
