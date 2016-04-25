ad3be2cf4562fb6b1dfc84662785f4a8511eaef2
Radu Matei <matei.radu94@gmail.com>
2016-04-25 09:29:56 +0300

    Added support for token in query string and connection header and refactored OAuthBearerTokenAuthenticationProvider

--- AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Providers/OAuthBearerTokenAuthenticationProvider.cs
+++ AspNetFundamentals/aspnet04 - SimpleTokenAuthentication/Providers/OAuthBearerTokenAuthenticationProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Owin.Security.OAuth;
 using System;
+using System.Linq;
 
 namespace SimpleTokenAuthentication.Providers
 {
@@ -8,10 +9,45 @@ namespace SimpleTokenAuthentication.Providers
     {
         public override Task RequestToken(OAuthRequestTokenContext context)
         {
-            var tokenCookie = context.OwinContext.Request.Cookies["BearerToken"];
+            string cookieToken = null;
+            string queryStringToken = null;
+            string headerToken = null;
 
-            if (!String.IsNullOrEmpty(tokenCookie))
-                context.Token = tokenCookie;
+            try
+            {
+                cookieToken = context.OwinContext.Request.Cookies["BearerToken"];
+            }
+            catch (NullReferenceException)
+            {
+                System.Diagnostics.Debug.WriteLine("The cookie does not contain the bearer token");
+            }
+
+            try
+            {
+                queryStringToken = context.OwinContext.Request.Query["BearerToken"].ToString();
+            }
+            catch (NullReferenceException)
+            {
+               System.Diagnostics.Debug.WriteLine("The query string does not contain the bearer token");
+            }
+
+            try
+            {
+                headerToken = context.OwinContext.Request.Headers["BearerToken"];
+            }
+            catch (NullReferenceException)
+            {
+                System.Diagnostics.Debug.WriteLine("The connection header does not contain the bearer token");
+            }
+
+            if (!String.IsNullOrEmpty(cookieToken))
+                context.Token = cookieToken;
+
+            else if (!String.IsNullOrEmpty(queryStringToken))
+                context.Token = queryStringToken;
+
+            else if (!String.IsNullOrEmpty(headerToken))
+                context.Token = headerToken;
 
             return Task.FromResult<object>(null);
         }
